b44a542c41856c6797f8ab793c796d7d60b30e71
Michael <Mkoerber7@gmail.com>
2018-03-27 22:05:44 -0500

    Styled Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header'
+import About from './components/About/About'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Portfolio</h1>
-        </header>
+      <div>
+        <Header />
+        <About />
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+        return (
+            <div className="header-container">
+                <h1 className="header-name">Michael Koerber</h1>
+                <p className="header-title">Web Developer</p>
+            </div>
+        )
+}
+
+export default Header;
