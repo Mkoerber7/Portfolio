cd91cb9f37e12217a5454365699bfbcb115a16ab
Michael <Mkoerber7@gmail.com>
2018-03-29 16:54:23 -0500

    Added the basic structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header'
-import About from './components/About/About'
+import About from './components/About/About';
+import Skills from './components/Skills/Skills';
+import Projects from "./components/Projects/Projects"
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
@@ -8,6 +11,9 @@ class App extends Component {
       <div>
         <Header />
         <About />
+        <Skills />
+        <Projects />
+        <Footer />
       </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Header = () => {
         return (
             <div className="header-container">
+              <div className="text-container">
                 <h1 className="header-name">Michael Koerber</h1>
                 <p className="header-title">Web Developer</p>
+              </div>
             </div>
         )
 }
