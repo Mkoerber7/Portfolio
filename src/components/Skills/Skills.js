7ec0cc506cbc7d394a41d4082e9cd74968382c03
Michael <Mkoerber7@gmail.com>
2018-04-01 21:40:45 -0500

    Added content and fully styled skills section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Skills = () => {
+
+const Skills = (props) => {
     return (
         <div className="skills-container">
-        
+         <h1 className="skills-title">My Skills</h1>
+         <div className="border-line"></div>
+         <section className="icons">
+            <i className="icon-html5-alt icon"></i>
+            <i className="icon-css3-alt icon"></i>
+            <i className="icon-sass icon"></i>
+            <i className="icon-javascript-alt icon"></i>
+            <i className="icon-nodejs icon"></i>
+            <i className="icon-reactjs icon"></i>
+            <i className="icon-git icon"></i>
+            <i className="icon-postgres icon"></i>
+         </section>
         </div>
     )
 }
