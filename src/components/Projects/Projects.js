4a4497fdb2ce2fc4f549a3f9d5c8b912cc8e404b
Michael <Mkoerber7@gmail.com>
2018-04-08 16:15:29 -0500

    Started styling projects sections and contract me

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,24 @@ import React from 'react';
 const Projects = () => {
     return (
         <div className="projects-container">
-        
+            <h1>Projects</h1>
+            <div className="all-projects">
+                <div className="rf-box box">
+                  <h2>Recipe Finder</h2>
+                </div>
+
+                <div className="sp-box box">
+                  <h2>Strickland Propane</h2>
+                </div>
+
+                <div className="cc-box box">
+                  <h2>Casa Connection</h2>
+                </div>
+
+                <div className="contact-box box">
+                 <h2>Contact Me</h2>
+                </div>
+            </div>
         </div>
     )
 }
