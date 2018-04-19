b6ce1c7f570088ddcbdaa51ff0eed0cb8e5cd26c
Michael <Mkoerber7@gmail.com>
2018-04-18 20:53:48 -0500

    First attemp at fixing height bug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header/Header'
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Projects from "./components/Projects/Projects"
-import Footer from './components/Footer/Footer';
 
 class App extends Component {
   render() {
