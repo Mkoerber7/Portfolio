83f97e8a32eb10e2cc45718f67454178e72447af
Michael <Mkoerber7@gmail.com>
2018-04-19 16:39:41 -0500

    Added currently learned in about and first attempt at fixing footer bug

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import portrait from '../../assets/portrait.png'
 
-const About = () => {
+const About = (props) => {
     return (
         <div id="about-container">
         <div className="image-div">
@@ -9,8 +9,11 @@ const About = () => {
         </div>
         <div className="about-div">
             <h2 className="about-title">Hello!</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        </div>
+            <p>I'm a web developer with an array of skills and a thirst for knowledge.  
+            </p>
+            <p className="currently">And I'm currently learning...</p>
+            <i className="icon-python icon"></i>
+            </div>
         
         </div>
     )
